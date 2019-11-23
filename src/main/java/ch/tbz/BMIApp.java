b34b827e4f0cb1e2bf15b00de2e41bd709833b80
Thomas Kälin <thomas.kaelin@tbz.ch>
2019-11-23 19:25:59 +0100

    Version 4.0

---
+++ src/main/java/ch/tbz/BMIApp.java
@@ -0,0 +1,30 @@
+package ch.tbz;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class BMIApp extends Application {
+
+    public static void main( String[] args )
+    {
+		launch(args);		
+	}
+
+    @Override
+	public void start(Stage primaryStage) throws Exception {
+				
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("BMIView.fxml"));
+		Parent root = loader.load();
+		//BMIController controller = loader.getController();
+			
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("BMISettings.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Calculate BMI");
+		primaryStage.show();
+	}
+
+}
